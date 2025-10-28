ccd9d8e9f5f2b5bd1fadef3db9c353cf4a2f2285
Samarth Lad <smrthlad@gmail.com>
2025-10-28 13:01:12 +0530

    Fix import path for AuthContext in SignIn component

--- frontend/src/component/SignIn.jsx
+++ frontend/src/component/SignIn.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom"; 
 import { FaEnvelope, FaLock, FaGoogle, FaFacebookF } from "react-icons/fa";
 import axios from "axios";
-import { useAuth } from "../context/Authcontext";
+import { useAuth } from "../context/AuthContext";
 import { toast } from "react-toastify";
 import ClipLoader from "react-spinners/ClipLoader";
 
